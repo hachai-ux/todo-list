629e8ff4c77ed0a3828b17f7ed8521fa67276ce5
Hac Hai Pham <hpham.ux@gmail.com>
2021-07-23 20:47:38 +0200

    adding projects and todos works

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //factory functions set up and return the new object when you call the function
 
-const TodoItemFactory = (title, description, dueDate, priority, notes) => {
+//todo items carry information about the todos
+const TodoFactory = (title, description, dueDate, priority, notes) => {
     const getTitle = () => title;
     const getDescription  = () => description;
     const getDueDate = () => dueDate;
@@ -9,30 +10,35 @@ const TodoItemFactory = (title, description, dueDate, priority, notes) => {
     return {getTitle, getDescription, getDueDate, getPriority, getNotes};
 };
  
+//projects carry name and a todo list
 const ProjectsFactory = (title) => {
-        let toDoList = [];
-        const getTitle = () => title;
-    return {title, toDoList};
+    let toDoList = [];
+    const getTitle = () => title;
+    const getToDoList = () => toDoList;
+    return {getTitle, getToDoList};
 };
 
-
-const DomController = () => {
-
-};
-
+//manages a list of projects
 const ProjectController = (() => {
     let projectList = [];
     const addProject = (project) => {
         projectList.push(project);
     };
-    const addToDo = (project, todo) => {
-        project.toDoList.push(todo);
-    };
     const getProjectList = () => projectList;
 
-    return{addProject, addToDo, getProjectList}
+    return{addProject, getProjectList}
 })();
 
+//manages the todo-list for each project
+const ToDoController = (() => {
+    //add a todo to a project
+    const addToDo = (todo, project) => {
+        project.getToDoList().push(todo);
+    };
+
+
+    return{addToDo}
+})();
 
 const OnLoad = (() => {
     const defaultProject = ProjectsFactory("Default Project");
@@ -43,4 +49,22 @@ const OnLoad = (() => {
 
 
 OnLoad.addDefaultProject();
+const project2 = ProjectsFactory("Project 2");
+const todo1 = TodoFactory("Name", "Description", "DueDate", "Priority", "Notes");
+//add a project to project list
+ProjectController.addProject(project2);
+//add todo to a project
+ToDoController.addToDo(todo1, project2);
 console.log(ProjectController.getProjectList());
+//get title of todo in project 2
+console.log(project2.getToDoList()[0].getTitle());
+
+
+
+//backlog:
+//make description, due date, priority and notes optional
+//set todos as complete
+//change todo priority
+//change name etc.
+
+//DOM modules
